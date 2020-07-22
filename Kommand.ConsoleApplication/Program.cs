d125762c6836a7ed1f691e361c1a8c195e34f593
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2020-07-22 18:13:05 +0400

    Add project files.

---
+++ Kommand.ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kommand.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                Console.WriteLine("Coomand:");
+                var s = Console.ReadLine();
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
